c6395ba637ba9dc75b9e777fc505e2af6b5caece
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-10-07 10:25:43 +0100

    NADAF-2 Qdd Cors middleware

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 const express = require('express');
 const userRoutes = require('./src/api/v1/routes/userRoutes');
 const { connect } = require('./config/config'); // Assurez-vous d'importer la fonction connect
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json()); // Pour parser le JSON
 
-connect(); // Appelez la fonction connect pour établir la connexion à MongoDB
-
+// Configuration de CORS pour autoriser votre frontend
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true 
+}));
+connect(); 
 // Utilisation des routes
 app.use('/api/v1/users', userRoutes);
 
